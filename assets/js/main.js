1f5b1483e85ae0893df661101ae5cacb69b0e086
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-05-14 16:30:55 -0400

    change email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,32 @@
-AOS.init();
+AOS.init()
 // You can also pass an optional settings object
 // below listed default settings
 AOS.init({
-  
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 700, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
+  easing: "ease", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
-
-
-const contact = document.querySelector('.contact')
-const contactbtn = document.querySelector('.contact-btn')
-const currentYear = document.querySelector('.current-year')
-
-contact.addEventListener('click', function(){
-window.location.href="mailto:matt.ehrlich93@yahoo.com"
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 })
 
-contactbtn.addEventListener('click', function(){
-window.location.href="mailto:matt.ehrlich93@yahoo.com"
+const contact = document.querySelector(".contact")
+const contactbtn = document.querySelector(".contact-btn")
+const currentYear = document.querySelector(".current-year")
+
+contact.addEventListener("click", function () {
+  window.location.href = "mailto:matt.ehrlich993@gmail.com"
+})
+
+contactbtn.addEventListener("click", function () {
+  window.location.href = "mailto:matt.ehrlich993@gmail.com"
 })
 
 const year = new Date().getFullYear()
 
-function returnYear(){
+function returnYear() {
   currentYear.innerHTML = year
 }
 returnYear()
